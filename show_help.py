b0ea227099c46f084c196ebd2fc4f91a5beacb45
yamajunn <amiton1115@gmail.com>
2023-11-23 01:21:57 +0900

    a

--- show_help.py
+++ show_help.py
@@ -1,7 +1,7 @@
 def show_help(OK_DICT, sp):
     print("<<ヘルプ一覧>>\n")
     for item in OK_DICT:
-        print(OK_DICT[item][0], end=f"{'　'*(15 - len(OK_DICT[item][0]))}")
+        print(f"{OK_DICT[item][1]}: {OK_DICT[item][0]}", end=f"{'　'*(15 - len(OK_DICT[item][0]))}")
         for i, list_item in enumerate(OK_DICT[item]):
             if i != 0:
                 print(list_item, end=",  ")
