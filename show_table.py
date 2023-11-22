efc1edb5ce1bd7ca86f16de904f71dea1ac5f4b9
yamajunn <amiton1115@gmail.com>
2023-11-23 00:27:23 +0900

    add show table

--- show_table.py
+++ show_table.py
@@ -1,10 +1,6 @@
-def show_help(OK_DICT, sp):
-    print("<<ヘルプ一覧>>\n")
-    for item in OK_DICT:
-        print(OK_DICT[item][0], end=f"{'　'*(15 - len(OK_DICT[item][0]))}")
-        for i, list_item in enumerate(OK_DICT[item]):
-            if i != 0:
-                print(list_item, end=",  ")
-        print()
+def show_table(cur, sp):
+    print("<<テーブル一覧>>\n")
+    cur.execute("select * from sqlite_master where type='table'")
+    for item in cur.fetchall():
+        print(item)
     print("="*sp)
-    
